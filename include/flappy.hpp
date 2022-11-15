85b7f015eddcc6b0a3ab1c965d623a9034c6648d
Robert O'Shea <robertoshea2k11@gmail.com>
2022-11-15 02:42:27 +0000

    Added top level header

---
+++ include/flappy.hpp
@@ -0,0 +1,12 @@
+#ifndef __FLAPPY_HPP__
+#define __FLAPPY_HPP__
+
+#include <string>
+
+#include <SDL.h>
+#include <SDL2/SDL_image.h>
+
+#include "rect.hpp"
+#include "sprite.hpp"
+
+#endif /* __FLAPPY_HPP__ */
